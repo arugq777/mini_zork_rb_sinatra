caa556fc9680dc4725cb7150e07640464b82bbfc
arugq777 <apathy.r.us@gmail.com>
2014-01-26 13:25:11 -0600

    made some slim templates. settings form doesn't seem to work yet

--- mz_sinatra_app.rb
+++ mz_sinatra_app.rb
@@ -4,6 +4,7 @@ gem "rubytree", "0.8.3"
 require "./lib/mzweb"
 require "sinatra"
 require "sinatra/partial"
+require "slim"
 require "haml"
 
 class MiniZorkApp < Sinatra::Base
@@ -25,24 +26,28 @@ class MiniZorkApp < Sinatra::Base
   get '/' do
     mzw = MiniZorkWeb.new({})
     reset_mzw(mzw)
+    slim :"/slim/index"
     # erb :"/erb/index"
-    haml :"/haml/index"
+    #haml :"/haml/index"
   end
 
   get '/exits' do
+    slim :"/slim/exits"
     # erb :"/erb/exits"
-    haml :"/haml/exits"
+    # haml :"/haml/exits"
   end
 
   get '/info' do
+    slim :"/slim/info"
     # erb :"/erb/info"
-    haml :"/haml/info"
+    # haml :"/haml/info"
 
   end
 
   get '/settings' do
+    slim :"/slim/settings"
     # erb :"/erb/settings"
-    haml :"/haml/settings"
+    # haml :"/haml/settings"
   end
 
   post '/' do
@@ -52,11 +57,13 @@ class MiniZorkApp < Sinatra::Base
         settings.mzw.play(command)
         if settings.mzw.output_hash[:move] == false
           @msg = "There is no exit to the #{command.to_s.upcase}!"
+          slim :"/slim/invalid_move", locals: {msg: @msg}
           # erb :"/erb/invalid_move", locals: {msg: @msg}
-          haml :"/haml/invalid_move", locals: {msg: @msg}
+          # haml :"/haml/invalid_move", locals: {msg: @msg}
         else
+          slim :"/slim/turn"
           # erb :"/erb/turn"
-          haml :"/haml/turn"
+          # haml :"/haml/turn"
         end
       end
     else
@@ -66,8 +73,9 @@ class MiniZorkApp < Sinatra::Base
       #puts "sss2", sess_settings
       new_mzw = MiniZorkWeb.new(session_settings)
       reset_mzw(new_mzw)
+      slim :"/slim/main"
       # erb :"/erb/main"
-      haml :"/haml/main"
+      # haml :"/haml/main"
     end
   end
 end
